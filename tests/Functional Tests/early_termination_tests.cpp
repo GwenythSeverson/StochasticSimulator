32670af015e243d79bd293376a0e491d5c8e1ada
GwenythSeverson <gwenyth.severson@gmail.com>
2026-07-27 12:56:06 -0500

    organization

--- tests/Functional Tests/early_termination_tests.cpp
+++
@@ -1,7 +0,0 @@
-#include "early_termination_tests.hpp"
-#include <cassert>
-#include <iostream>
-#include "general_functions.hpp"
-// GUnit?
-void run_early_termination_tests() {
-}
--- tests/Functional Tests/early_termination_tests.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-/**
- * Runs the early-termination behavior tests for stochastic helpers.
- */
-void run_early_termination_tests();
